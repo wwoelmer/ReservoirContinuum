e648fd62ad14e3a481401a1be075ce23d493a788
wwoel <wwoelmer@vt.edu>
2021-10-28 09:55:15 -0400

    more figure comparisons

--- Scripts/make_boxplots.R
+++ Scripts/make_boxplots.R
@@ -1,5 +1,4 @@
-# spearman's correlation across space and time or both
-
+# boxplots of variables across time and space
 
 library(tidyverse)
 library(patchwork)
@@ -67,15 +66,37 @@ labels <- c('Autoch', 'Alloch', 'DOC',
 names(labels) <- levels
 long$variable <- factor(long$variable, levels = levels)
 
-
-
+##########################################################################################################################
+### boxplots of concentrations
 ggplot(data = long, aes(x = variable, y = value, fill = as.factor(Reservoir))) +
   facet_wrap(~variable, scales = 'free') +
-  geom_boxplot()
+  geom_boxplot() +
+  geom_point(position=position_jitterdodge(),alpha=0.15) 
+  
 
-ggplot(data = long, aes(x = as.factor(distance_from_stream), y = value)) +
+ggplot(data = long, aes(x = as.factor(distance_from_stream), y = value, fill = as.factor(distance_from_stream))) +
   facet_wrap(~variable, scales = 'free') +
-  geom_boxplot()
+  geom_boxplot() +
+  geom_point(position=position_jitterdodge(),alpha=0.2) +
+  theme(legend.position = 'none')
+
+ggplot(data = long[long$distance_from_stream>0,], aes(x = as.factor(distance_from_stream), y = value, fill = as.factor(distance_from_stream))) +
+  facet_wrap(~variable, scales = 'free') +
+  geom_boxplot() +
+  geom_point(position=position_jitterdodge(),alpha=0.3) +
+  theme(legend.position = 'none')
+
+ggplot(data = long[long$distance_from_stream>0,], aes(x = as.factor(month(Date)), y = value, fill = as.factor(Date))) +
+  facet_wrap(~variable, scales = 'free') +
+  geom_boxplot() +
+  geom_point(position=position_jitterdodge(),alpha=0.3) +
+  theme(legend.position = 'none')
+
+ggplot(data = long, aes(x = as.factor(month(Date)), y = value, fill = as.factor(Date))) +
+  facet_wrap(~variable, scales = 'free') +
+  geom_boxplot() +
+  geom_point(position=position_jitterdodge(),alpha=0.2) +
+  theme(legend.position = 'none')
 
 ggplot(data = long[long$distance_from_stream>0,], aes(x = as.factor(distance_from_stream), 
                                                       y = value, 
@@ -93,13 +114,144 @@ ggplot(data = long[long$distance_from_stream>0,], aes(x = as.factor(month(Date))
   geom_jitter(width=0.1,alpha=0.2) +
   theme(legend.position = 'none')
 
-# make correlations for all
-unique(long$variable)
-for(i in 1:length(unique(long$variable))){
-  unique(long$variable[i])
-  x <- long[long$variable==unique(long$variable[i]),]
+##### and WITH the stream data
+ggplot(data = long, aes(x = as.factor(distance_from_stream), 
+                                                      y = value, 
+                                                      fill = as.factor(Reservoir))) +
+  facet_wrap(~variable, scales = 'free') +
+  geom_boxplot() +
+  geom_point(position=position_jitterdodge(),alpha=0.3) #+
+theme(legend.position = 'none')
+#geom_jitter(width=0.1,alpha=0.2)  
 
-}
-l <- rnorm(20, 2, 1)
-m <- rnorm(20, 1, 2)
-cor.test(l, m, use = 'complete.obs', method = 'spearman')
+
+ggplot(data = long, aes(x = as.factor(month(Date)), y = value, fill = as.factor(Reservoir))) +
+  facet_wrap(~variable, scales = 'free') +
+  geom_boxplot() +
+  geom_point(position=position_jitterdodge(),alpha=0.2) #+
+  theme(legend.position = 'none')
+
+
+
+#########################################################################################################################
+#### plot CV as boxplot
+
+# calculate heterogeneity within the reservoir (coefficient of variation) for each reservoir on each RC day
+
+CV_time <- data[data$distance_from_stream>0,] %>% group_by(Reservoir, Site) %>% 
+  mutate(CV_chl = cv(Chla_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_SRP = cv(SRP_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_NO3 = cv(NO3NO2_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_NH4 = cv(NH4_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TP = cv(TP_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TN = cv(TN_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TNTP = cv(TN_TP, na.rm = TRUE)) %>% 
+  mutate(CV_A = cv(A, na.rm = TRUE)) %>% 
+  mutate(CV_T = cv(A, na.rm = TRUE)) %>% 
+  mutate(CV_HIX = cv(HIX, na.rm = TRUE)) %>% 
+  mutate(CV_BIX = cv(BIX, na.rm = TRUE)) %>% 
+  mutate(CV_DPTP = cv(DP_TP, na.rm = TRUE)) %>% 
+  mutate(CV_DNTN = cv(DN_TN, na.rm = TRUE)) %>% 
+  select(Date, Reservoir, distance_from_stream, distance_m, CV_chl:CV_DNTN) %>% 
+  distinct(Site, Reservoir, .keep_all = TRUE)
+CV_time$Date <- as.Date(CV_time$Date)
+
+time_long <- CV_time %>% 
+  pivot_longer(CV_chl:CV_DNTN, names_to = 'variable', values_to = 'cv')
+time_long$axis <- 'time'
+
+CV_site <- data[data$distance_from_stream>0,] %>% group_by(Reservoir, Date) %>% 
+  mutate(CV_chl = cv(Chla_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_SRP = cv(SRP_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_NO3 = cv(NO3NO2_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_NH4 = cv(NH4_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TP = cv(TP_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TN = cv(TN_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TNTP = cv(TN_TP, na.rm = TRUE)) %>% 
+  mutate(CV_A = cv(A, na.rm = TRUE)) %>% 
+  mutate(CV_T = cv(A, na.rm = TRUE)) %>% 
+  mutate(CV_HIX = cv(HIX, na.rm = TRUE)) %>% 
+  mutate(CV_BIX = cv(BIX, na.rm = TRUE)) %>% 
+  mutate(CV_DPTP = cv(DP_TP, na.rm = TRUE)) %>% 
+  mutate(CV_DNTN = cv(DN_TN, na.rm = TRUE)) %>% 
+  select(Date, Reservoir, distance_from_stream, distance_m, CV_chl:CV_DNTN) %>% 
+  distinct(Date, Reservoir, .keep_all = TRUE)
+CV_site$Date <- as.Date(CV_site$Date)
+
+site_long <- CV_site %>% 
+  pivot_longer(CV_chl:CV_DNTN, names_to = 'variable', values_to = 'cv')
+site_long$axis <- 'space'
+
+## plots
+ggplot(time_long, aes(x = as.factor(Site), y = cv, fill = Reservoir)) +
+  geom_bar(position = 'dodge', stat = 'identity') +
+  facet_wrap(~variable, scale = 'free') 
+
+ggplot(site_long, aes(x = as.factor(month(Date)), y = cv, fill = Reservoir)) +
+  geom_bar(position = 'dodge', stat = 'identity') +
+  facet_wrap(~variable, scale = 'free')
+
+
+# combine
+long_both <- full_join(time_long, site_long)
+
+ggplot(long_both, aes(x = as.factor(axis), y = cv, fill = Reservoir)) +
+  geom_boxplot() +
+  facet_wrap(~variable, scale = 'free') +
+  geom_point(position=position_jitterdodge(),alpha=0.3) +
+  ggtitle('just in-reservoir data')
+  
+
+####### do again WITH stream data
+CV_time <- data %>% group_by(Reservoir, Site) %>% 
+  mutate(CV_chl = cv(Chla_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_SRP = cv(SRP_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_NO3 = cv(NO3NO2_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_NH4 = cv(NH4_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TP = cv(TP_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TN = cv(TN_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TNTP = cv(TN_TP, na.rm = TRUE)) %>% 
+  mutate(CV_A = cv(A, na.rm = TRUE)) %>% 
+  mutate(CV_T = cv(A, na.rm = TRUE)) %>% 
+  mutate(CV_HIX = cv(HIX, na.rm = TRUE)) %>% 
+  mutate(CV_BIX = cv(BIX, na.rm = TRUE)) %>% 
+  mutate(CV_DPTP = cv(DP_TP, na.rm = TRUE)) %>% 
+  mutate(CV_DNTN = cv(DN_TN, na.rm = TRUE)) %>% 
+  select(Date, Reservoir, distance_from_stream, distance_m, CV_chl:CV_DNTN) %>% 
+  distinct(Site, Reservoir, .keep_all = TRUE)
+CV_time$Date <- as.Date(CV_time$Date)
+
+time_long <- CV_time %>% 
+  pivot_longer(CV_chl:CV_DNTN, names_to = 'variable', values_to = 'cv')
+time_long$axis <- 'time'
+
+CV_site <- data %>% group_by(Reservoir, Date) %>% 
+  mutate(CV_chl = cv(Chla_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_SRP = cv(SRP_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_NO3 = cv(NO3NO2_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_NH4 = cv(NH4_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TP = cv(TP_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TN = cv(TN_ugL, na.rm = TRUE)) %>% 
+  mutate(CV_TNTP = cv(TN_TP, na.rm = TRUE)) %>% 
+  mutate(CV_A = cv(A, na.rm = TRUE)) %>% 
+  mutate(CV_T = cv(A, na.rm = TRUE)) %>% 
+  mutate(CV_HIX = cv(HIX, na.rm = TRUE)) %>% 
+  mutate(CV_BIX = cv(BIX, na.rm = TRUE)) %>% 
+  mutate(CV_DPTP = cv(DP_TP, na.rm = TRUE)) %>% 
+  mutate(CV_DNTN = cv(DN_TN, na.rm = TRUE)) %>% 
+  select(Date, Reservoir, distance_from_stream, distance_m, CV_chl:CV_DNTN) %>% 
+  distinct(Date, Reservoir, .keep_all = TRUE)
+CV_site$Date <- as.Date(CV_site$Date)
+
+site_long <- CV_site %>% 
+  pivot_longer(CV_chl:CV_DNTN, names_to = 'variable', values_to = 'cv')
+site_long$axis <- 'space'
+
+# combine
+long_both <- full_join(time_long, site_long)
+
+ggplot(long_both, aes(x = as.factor(axis), y = cv, fill = Reservoir)) +
+  geom_boxplot() +
+  facet_wrap(~variable, scale = 'free') +
+  geom_point(position=position_jitterdodge(),alpha=0.3) +
+  ggtitle('including stream data')
