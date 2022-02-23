a1f80f2cb365267d22f3291631ad7b99af252f7a
wwoel <wwoelmer@vt.edu>
2022-02-23 16:01:53 -0500

    move script to make fig 3

--- Scripts/03_make_time_series_fig3.R
+++ Scripts/03_make_time_series_fig3.R
@@ -11,73 +11,33 @@ r_col <- c('olivedrab3', 'royalblue1')
 
 
 #data <- read.csv('./Data/continuum_pf.csv')
-data <-read.csv('./Data/continuum_ww.csv')
-data$Date <- as.Date(data$DateTime)
-data$connectivity <- as.factor(data$connectivity)
-
-data <- data %>% select(Date, everything(),-DateTime, -Depth_m, -(DIC_mgL:Flag_DN))
+data <-read.csv('./Data/continuum_data.csv')
+data$Date <- as.Date(data$Date)
+data$distance_from_stream <- as.numeric(data$distance_from_stream)
 
 # some SRP values are 0, so add very very small value to these for dividing by this value later
-min_val <- min(data[data$SRP_ugL>0,"SRP_ugL"])
-data$SRP_ugL <- data$SRP_ugL + min_val
-
-
-data <- data %>% 
-  mutate(TN_TP = TN_ugL/TP_ugL) %>% 
-  mutate(DP_TP = SRP_ugL/TP_ugL) %>% 
-  mutate(DN_TN = (NH4_ugL + NO3NO2_ugL)/TN_ugL) %>% 
-  mutate(DN_DP = (NH4_ugL + NO3NO2_ugL)/SRP_ugL)
-
-
-data$distance_from_stream <- 'NA'
-data <- data %>% unite('res_site', Reservoir:Site, remove = FALSE)
-stream_sites <- c('FCR_100', 'FCR_200', 'FCR_102', 'FCR_101', 'FCR_99', 'BVR_100', 'BVR_200', 'FCR_1')
-riv_sites <- c('FCR_20', 'BVR_20', 'BVR_30') 
-trans_sites <- c('BVR_1', 'BVR_45', 'FCR_30', 'FCR_45')
-lac_sites <- c('FCR_50', 'BVR_50')
-
-for (i in 1:nrow(data)) {
-  if(data$res_site[i] %in% stream_sites){
-    data$distance_from_stream[i] <- '0'
-  }else if(data$res_site[i] %in% riv_sites){
-    data$distance_from_stream[i] <- '1'
-  }else if(data$res_site[i] %in% trans_sites){
-    data$distance_from_stream[i] <- '2'
-  }else if(data$res_site[i] %in% lac_sites){
-    data$distance_from_stream[i] <- '3'
-  }
-}
-
-data$distance_from_stream <- as.numeric(data$distance_from_stream)
-in_reservoir <- c( '20', '30', '45', '50') # site 01 is in res for BVR and in stream for FCR 
-in_stream <- c('99', '100', '101', '102', '200')
-
-### read in distance data from arcgis
-dist <- read.csv('./Data/rc_coordinates_100only.csv')
-dist <- dist %>% select(Reservoir, Site, distance_m)
-
-data <- left_join(data, dist)
+#min_val <- min(data[data$SRP_ugL>0,"SRP_ugL"])
+#data$SRP_ugL <- data$SRP_ugL + min_val
 
 long <- data %>%   
-  select(Site, Reservoir, Date, distance_from_stream, TN_ugL:Chla_ugL, A:BIX, TN_TP:distance_m) %>% 
-  pivot_longer(TN_ugL:DN_DP, names_to = 'variable', values_to = 'value')
+  select(Site, Reservoir, Date, distance_from_stream, distance_m, Flow_cms, TN_ugL:T) %>% 
+  pivot_longer(TN_ugL:T, names_to = 'variable', values_to = 'value')
 
 vars_keep <-  c('T', 'A', 
                 'DOC_mgL',  'Chla_ugL','TN_ugL', 'TP_ugL',
-                'NH4_ugL', 'NO3NO2_ugL', 'SRP_ugL') #'HIX', 'BIX'
+                'NH4_ugL', 'NO3NO2_ugL', 'SRP_ugL') 
 vars_stoich <-  c('TN_TP', 'DN_DP', 'DP_TP', 'DN_TN')
 stoich <- long[long$variable %in% vars_stoich,]
 
 long <- long[long$variable %in% vars_keep,]
 
-levels <- c('T', 'A', 'DOC_mgL',  
-            'NH4_ugL', 'NO3NO2_ugL', 'SRP_ugL',
-            'Chla_ugL','TN_ugL', 'TP_ugL')
-            #'TN_TP', 'DP_TP', 'DN_TN') #'HIX', 'BIX'
-labels <- c('T-autoch',  'A-alloch', 'DOC',
-            'NH4', 'NO3', 'SRP',
-            'Chl-a', 'TN', 'TP') #'BIX-autoch','HIX-alloch',
-            #'TN_TP', 'DP_TP', 'DN_TN')
+levels <- c('NH4_ugL', 'NO3NO2_ugL', 'SRP_ugL',
+            'DOC_mgL',  'TN_ugL', 'TP_ugL',
+            'Chla_ugL','T', 'A')
+
+labels <- c('NH4', 'NO3', 'SRP',
+            'DOC', 'TN', 'TP',
+            'Chl-a', 'T-autoch',  'A-alloch') 
 names(labels) <- levels
 long$variable <- factor(long$variable, levels = levels)
 long$Month <- as.factor(month(long$Date))
@@ -86,27 +46,41 @@ long$Month <- as.factor(month(long$Date))
 ### time series color by date, one for each reservoir
 
 ## FCR
+labels_f <- c('d) NH4 (μg/L)', 'e) NO3 (μg/L)', 'f) SRP (μg/L)',
+            'j) DOC (mg/L)', 'k) TN (μg/L)', 'l) TP (μg/L)',
+            'p) Chl-a (μg/L)', 'q) T-autoch (RFU)',  'r) A-alloch (RFU)') 
+names(labels_f) <- levels
+long$variable <- factor(long$variable, levels = levels)
+long$Month <- as.factor(month(long$Date))
+
+
 f <- ggplot(data = long[long$Reservoir=='FCR' & long$distance_from_stream > 0,], aes(x = distance_m, y = value, col = Month)) +
   geom_point(size = 2) +
-  geom_smooth(aes(col = Month), alpha = 0.5) +
+  geom_smooth(aes(col = Month), se = FALSE) +
   xlab('Distance from stream (m)') +
   ylab('Concentration') +
-  facet_wrap(~variable, scales = "free_y", labeller = labeller(variable = labels)) + 
+  facet_wrap(~variable, scales = "free_y", labeller = labeller(variable = labels_f)) + 
   scale_color_manual(values = rev(hcl.colors(7, "Zissou 1"))) +
   theme_bw() +
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank()) +
   ggtitle('Falling Creek Reservoir')
 # concentration units: add in figure caption?
-
+f
 
 ## BVR
+labels_b <- c('a) NH4 (μg/L)', 'b) NO3 (μg/L)', 'c) SRP (μg/L)',
+              'g) DOC (mg/L)', 'h) TN (μg/L)', 'i) TP (μg/L)',
+              'm) Chl-a (μg/L)', 'n) T-autoch (RFU)',  'o) A-alloch (RFU)') 
+names(labels_b) <- levels
+long$variable <- factor(long$variable, levels = levels)
+long$Month <- as.factor(month(long$Date))
 b <- ggplot(data = long[long$Reservoir=='BVR' & long$distance_from_stream > 0,], aes(x = distance_m, y = value, col = Month)) +
   geom_point(size = 2) +
-  geom_smooth(aes(col = Month)) +
+  geom_smooth(aes(col = Month), se = FALSE) +
   xlab('Distance from stream (m)') +
   ylab('Concentration') +
-  facet_wrap(~variable, scales = "free_y", labeller = labeller(variable = labels)) + 
+  facet_wrap(~variable, scales = "free_y", labeller = labeller(variable = labels_b)) + 
   scale_color_manual(values = rev(hcl.colors(7, "Zissou 1"))) +
   theme_bw() +
   labs(col = 'Month') +
@@ -114,9 +88,41 @@ b <- ggplot(data = long[long$Reservoir=='BVR' & long$distance_from_stream > 0,],
         panel.grid.minor = element_blank(),
         legend.position = "none") +
   ggtitle('Beaverdam Reservoir')
-
+b
 ggarrange(b, f, nrow = 1, ncol = 2, common.legend = TRUE, legend = 'right') 
 
+
+#################################################################################################################
+# make Thornton figure with observed data
+thorn <- data %>% 
+  select(Date:Site, distance_from_stream, TN_ugL:SRP_ugL) %>% 
+  filter(res_site != 'FCR_1' & res_site != 'FCR_100' & res_site != 'FCR_101' & res_site != 'FCR_102') %>% 
+  filter(res_site != 'FCR_99' & res_site != 'FCR_200' & res_site != 'BVR_100' & res_site != 'BVR_200')
+
+thorn <- thorn %>% 
+  group_by(Reservoir, distance_from_stream) %>% 
+  mutate(total_n = mean(TN_ugL, na.rm = TRUE)) %>% 
+  mutate(total_p = mean(TP_ugL, na.rm = TRUE)) %>% 
+  mutate(soluble_p = mean(SRP_ugL, na.rm = TRUE)) %>% 
+  mutate(soluble_n = mean((NH4_ugL + NO3NO2_ugL), na.rm = TRUE)) %>% 
+  select(res_site, Reservoir, Site, distance_from_stream, total_n:soluble_n) %>% 
+  distinct(res_site, .keep_all = TRUE)
+
+thorn <- thorn %>% 
+  pivot_longer(total_n:soluble_n, names_to = 'variable', values_to = 'value')
+
+ggplot(data = thorn, aes(x = distance_from_stream, y = value, col = Reservoir)) +
+  geom_point(size = 2) +
+  #geom_smooth(aes(col = Reservoir), se = FALSE) +
+  geom_line(size = 2) +
+  xlab('Distance from stream (m)') +
+  ylab('Concentration') +
+  facet_wrap(~variable, scales = "free_y") + 
+  scale_color_manual(values = rev(hcl.colors(7, "Zissou 1"))) +
+  theme_bw() +
+  labs(col = 'Reservoir') +
+  theme(panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank()) 
 
 ############################################################################################################
 ### stoiciometry figure
@@ -131,7 +137,7 @@ stoich$variable <- factor(stoich$variable, levels = levels)
 ## FCR
 fs <- ggplot(data = stoich[stoich$Reservoir=='FCR' & stoich$distance_from_stream > 0,], aes(x = distance_m, y = value, col = Month)) +
   geom_point(size = 2) +
-  geom_smooth(aes(col = Month)) +
+  geom_smooth(aes(col = Month), se = FALSE) +
   xlab('Distance from stream (m)') +
   ylab('Concentration') +
   facet_wrap(~variable, scales = "free_y") + 
@@ -146,7 +152,7 @@ fs <- ggplot(data = stoich[stoich$Reservoir=='FCR' & stoich$distance_from_stream
 ## BVR
 bs <- ggplot(data = stoich[stoich$Reservoir=='BVR' & stoich$distance_from_stream > 0,], aes(x = distance_m, y = value, col = Month)) +
   geom_point(size = 2) +
-  geom_smooth(aes(col = Month)) +
+  geom_smooth(aes(col = Month), se = FALSE) +
   xlab('Distance from stream (m)') +
   ylab('Concentration') +
   facet_wrap(~variable, scales = "free_y") + 
@@ -160,6 +166,12 @@ bs <- ggplot(data = stoich[stoich$Reservoir=='BVR' & stoich$distance_from_stream
 
 
 ggarrange(bs, fs, nrow = 1, ncol = 2, common.legend = TRUE, legend = 'right') 
+
+
+
+
+
+
 
 
 
