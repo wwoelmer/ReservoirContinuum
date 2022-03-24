6ac9bb984a1a633cf1302ca99e0bf8d9809abc4a
wwoel <wwoelmer@vt.edu>
2022-03-24 13:42:43 -0400

    format wrt figure

--- Scripts/07_calculate_WRT.R
+++ Scripts/07_calculate_WRT.R
@@ -28,6 +28,7 @@ bvr <- bvr %>% group_by(Date) %>% summarise_all(funs(sum)) %>% filter(Site=="300
 # Calculate WRT
 # FCR: Assume full pond volume (3.1E5 m3)
 fcr <- fcr %>% mutate(wrt_d = (3.1E5/Flow_cms)*(1/60)*(1/60)*(1/24))
+fcr$Date <- as.Date(fcr$Date)
 mean(fcr$wrt_d)
 sd(fcr$wrt_d)
 std.error(fcr$wrt_d)
@@ -35,6 +36,7 @@ std.error(fcr$wrt_d)
 # BVR: Averaged water level for 2019 (10.91m) which corresponds to 8.9E5 m3
 # maybe need to use variable volume? water level data here: https://docs.google.com/spreadsheets/d/1XLJu5T7qsWtbQDBDEv3_63yfNB_GVXgs/edit#gid=353013140
 bvr <- bvr %>% mutate(wrt_d = (8.9E5/Flow_cms)*(1/60)*(1/60)*(1/24))
+bvr$Date <- as.Date(bvr$Date)
 mean(bvr$wrt_d)
 sd(bvr$wrt_d)
 std.error(bvr$wrt_d)
@@ -45,7 +47,10 @@ ggplot()+
   geom_point(bvr,mapping=aes(x=Date,y=wrt_d,color='BVR'),size=2)+
   geom_line(fcr,mapping=aes(x=Date,y=wrt_d,color='FCR',group=1),size=1)+
   geom_point(fcr,mapping=aes(x=Date,y=wrt_d,color='FCR'),size=2)+
-  theme_classic(base_size=15)
+  theme_classic(base_size=15) +
+  ylab('Residence Time (Days)') +
+  labs(color = 'Reservoir') +
+  scale_x_date(date_breaks = "months", date_labels = "%b")
 
 write.csv(wrt,"./Data/RC_WRT.csv")
 
