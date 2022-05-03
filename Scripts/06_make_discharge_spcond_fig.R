a25af0b8200aaacdf6d9359c5a832a86a9be50cd
wwoel <wwoelmer@vt.edu>
2022-05-03 10:32:16 -0400

    simple site shapes in fig

--- Scripts/06_make_discharge_spcond_fig.R
+++ Scripts/06_make_discharge_spcond_fig.R
@@ -55,13 +55,37 @@ flow <- flow %>%
   mutate(name = ifelse(res_site=="FCR_99", '100', Site)) %>% 
   mutate(labels = as.factor(Site))
 
+# add the simple site labels
+flow$simple_site <- NA
+flow$simple_site <- factor(flow$simple_site, levels = c('S1', 'S2', 'Out'))
+for(i in 1:nrow(flow)){
+  if(flow$res_site[i]=='BVR_100'){
+    flow$simple_site[i] <- 'S1'
+  }
+  if(flow$res_site[i]=='BVR_200'){
+    flow$simple_site[i] <- 'S2'
+  }
+  if(flow$res_site[i]=='BVR_50'){
+    flow$simple_site[i] <- 'Out'
+  }
+  if(flow$res_site[i]=='FCR_99'){
+    flow$simple_site[i] <- 'S2'
+  }
+  if(flow$res_site[i]=='FCR_200'){
+    flow$simple_site[i] <- 'S1'
+  }
+  if(flow$res_site[i]=='FCR_50'){
+    flow$simple_site[i] <- 'Out'
+  }
+}
+
 inf <- ggplot(data = flow, aes(x = Date, y = Flow_cms)) +
-  geom_point(aes(col = Reservoir, shape = res_site), size = 4) +
-  geom_line(data = flow[flow$Reservoir=='FCR',], aes(shape = res_site, col = Reservoir)) +
-  geom_line(data = flow[flow$Reservoir=='BVR',], aes(shape = res_site, col = Reservoir)) +
+  geom_point(aes(col = Reservoir, shape = simple_site), size = 4) +
+  geom_line(data = flow[flow$Reservoir=='FCR',], aes(shape = simple_site, col = Reservoir)) +
+  geom_line(data = flow[flow$Reservoir=='BVR',], aes(shape = simple_site, col = Reservoir)) +
   theme_bw() +
   scale_color_manual(values = r_col) +
-  scale_shape_manual(values = c(15, 0, 1, 2, 16, 17)) +
+  scale_shape_manual(values = c(15, 17, 16)) +
   ylab('Discharge (m^3/s)') +
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
@@ -74,23 +98,47 @@ inf
 data <- data %>% 
   mutate(labels = Site)
 
+# add the simple site labels
+data$simple_site <- NA
+data$simple_site <- factor(data$simple_site, levels = c('S1', 'S2', 'In-Reservoir'))
+for(i in 1:nrow(data)){
+  if(data$res_site[i]=='BVR_100'){
+    data$simple_site[i] <- 'S1'
+  }
+  if(data$res_site[i]=='BVR_200'){
+    data$simple_site[i] <- 'S2'
+  }
+  if(data$res_site[i] %in% c('BVR_1', 'BVR_20', 'BVR_30', 'BVR_45', 'BVR_50',
+                             'FCR_20', 'FCR_30', 'FCR_45', 'FCR_50')){
+    data$simple_site[i] <- 'In-Reservoir'
+  }
+  if(data$res_site[i]=='FCR_99'){
+    data$simple_site[i] <- 'S2'
+  }
+  if(data$res_site[i]=='FCR_200'){
+    data$simple_site[i] <- 'S1'
+  }
+}
+
 # calculate avg diff btw stream and in-reservoir sites
 diff <- data %>% 
   group_by(location, Date, Reservoir) %>% 
   mutate(mean_spc = mean(data$Sp_cond_uScm, na.rm = TRUE)) #%>% 
   distinct(location, Date, Reservoir, .keep_all = TRUE) %>% 
   select(Date, Reservoir, location, mean_spc)
+  
+
 
 spc <-  ggplot(data = data, aes(x = distance_m, y = Sp_cond_uScm)) +
   geom_point(data = data[data$distance_from_stream > 0,], aes(col = as.factor(month(Date))), size = 4) +
-  geom_point(data = data[data$distance_from_stream < 1,], aes(col = as.factor(month(Date)), shape = res_site), size = 4) +
+  geom_point(data = data[data$distance_from_stream < 1,], aes(col = as.factor(month(Date)), shape = simple_site), size = 4) +
   geom_line(data = data[data$Site!='200',], aes(col = as.factor(month(Date)))) +
   geom_line(data = data[data$Site!='100' & data$Site!='99',], aes(col = as.factor(month(Date)))) +
   facet_wrap(~Reservoir, scales = 'free_x') +
   #geom_label(aes(label = labels), na.rm = TRUE) +
   scale_size(guide = 'none') +
   theme_bw() +
-  scale_shape_manual(values = c(15, 0, 2, 17)) +
+  scale_shape_manual(values = c(15, 17, 16)) +
   theme(panel.grid.major = element_blank(), 
         panel.grid.minor = element_blank(),
         text = element_text(size = 15)) +
@@ -102,6 +150,7 @@ spc <-  ggplot(data = data, aes(x = distance_m, y = Sp_cond_uScm)) +
 spc
 
 sp_flow <- ggarrange(inf, spc, nrow = 1, ncol = 2, legend = 'right', labels = c('a', 'b')) 
+sp_flow
 ggsave('./Figures/Fig4_spcond_flow.png', sp_flow)
 
 ##################################################################################################################
